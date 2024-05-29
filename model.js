27e002ade586f8ed7742eb1e8822921501bab836
shuvamdaswebskitters <164153715+shuvamdaswebskitters@users.noreply.github.com>
2024-05-29 15:47:32 +0530

    Update model.js

--- model.js
+++ model.js
@@ -24,6 +24,13 @@ if ($("#bg_parent").length) {
           this.renderer = new THREE.WebGLRenderer({
             alpha: true,
             antialias: true,
+            precision: "mediump",
+            preserveDrawingBuffer: true,
+            powerPreference: "high-performance",
+            failIfMajorPerformanceCaveat: true,
+            stencil: 8,
+            depth: false,
+            logarithmicDepthBuffer: THREE.FloatType,
           });
           this.renderer.setPixelRatio(window.devicePixelRatio);
           this.renderer.setSize(content.clientWidth, content.clientHeight);
